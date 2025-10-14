e1b230e44c2fc9880fc202149f734fe9f2108c4c
Ilona-V <ilonova09@gmail.com>
2025-10-14 14:07:19 +0300

    add new funcion assignBook and update drygoe

--- main.go
+++ main.go
@@ -3,24 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	iduser1:=1
-	user1 :=&Reader{
-		ID:&iduser1,
-		FirstName:"g",
-		LastName:"gg",
-		isActive:true,
+	iduser1 := 1
+	user1 := &Reader{
+		ID:        &iduser1,
+		FirstName: "g",
+		LastName:  "gg",
+		isActive:  true,
 	}
 	fmt.Println(user1)
 
-	book1 :=&Book{
-		ID:1,
-		Title:"война и мир",
-		Year:1985,
-		Author:"tolstoy",
-		isIssue:false,
-		ReaderTakerID:0,
+	book1 := &Book{
+		ID:            1,
+		Title:         "война и мир",
+		Year:          1985,
+		Author:        "tolstoy",
+		isIssue:       false,
+		ReaderTakerID: 0,
 	}
-
 
 	fmt.Println(book1)
 	book1.IssueBook(user1)
