64f5e5adcef19e8b1ecc6dd13373893cf1bdb6c7
Ilona-V <ilonova09@gmail.com>
2025-10-14 12:26:58 +0300

    commit

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world!")
+	user1 :=Reader(
+		ID:1,
+		FirstName:"g",
+		LastName:"gg",
+		isActive:true
+	)
 }
--- models.go
+++ models.go
@@ -10,16 +10,26 @@ type Reader struct{
 }
 
 func (r Reader) DisplayReader(){
-	fmt.Printf("читатель:%s %s (ID: %s)\n",r.FirstName, r.LastName, r.ID)
+	fmt.Printf("читатель:%s %s (ID: %d)\n",r.FirstName, r.LastName, r.ID)
 }
 
 func (r *Reader) Deactive(){
 	r.isActive = false
 }
 
+func (r Reader) String() string{
+	status :=""
+	if(r.isActive){
+		status="active"
+	}else{
+		status="noactive"
+	}
+	return fmt.Sprintf("пользователь %s %s, id: %d, %s",r.FirstName, r.LastName, r.ID,status)
+}
+
 type Book struct{
 	ID int
 	Title string
 	Author string
-	isIssue string
+	isIssue bool
 }
